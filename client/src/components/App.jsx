020cbb5a854e0eb92447e184749549ad031fa099
florian-morel22 <florian.morel@ensta-paris.fr>
2023-03-31 14:39:28 +0200

    nouvelle version sans vite.js

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Banner from './Banner';
+import Body from './Body';
+
+
+function App() {
+
+  return (
+    <div className="App">
+        <Banner/>
+        <Body/>
+    </div>
+  );
+}
+
+export default App;
