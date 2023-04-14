66fc787be6db7db023620e1f11df9701328d67a9
florian-morel22 <florian.morel@ensta-paris.fr>
2023-04-14 16:40:29 +0200

    cocou Tao

--- src/components/Publication/PubliForm.jsx
+++ src/components/Publication/PubliForm.jsx
@@ -1,7 +1,19 @@
 const PubliForm = () => {
     return (
         <div className="pub-form-container">
-            <form></form>
+            <form>
+                <label>
+                    Nom de l'article <input type="text" />
+                </label>
+
+                <label>
+                    Description <input type="text" />
+                </label>
+
+                <label>
+                    Auteur <input type="text" />
+                </label>
+            </form>
         </div>
     );
 };
